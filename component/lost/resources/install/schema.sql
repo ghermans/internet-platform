bb442f04ddee80f1e5c06b7e4a4af946b74ffaab
Tom Janssens <tom@timble.net>
2013-12-18 14:12:06 +0100

    Added basic com:lost SQL schema

--- component/lost/resources/install/schema.sql
+++ component/lost/resources/install/schema.sql
@@ -1,7 +1,5 @@
 CREATE TABLE `lost_objects` (
   `lost_object_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `categories_category_id` int(11) NOT NULL DEFAULT '0',
-  `attachments_attachment_id` int(11) unsigned NOT NULL DEFAULT '0',
   `title` varchar(255) NOT NULL,
   `slug` varchar(255) DEFAULT NULL,
   `text` mediumtext NOT NULL,
@@ -12,8 +10,5 @@ CREATE TABLE `lost_objects` (
   `modified_on` datetime DEFAULT NULL,
   `locked_by` int(11) unsigned DEFAULT NULL,
   `locked_on` datetime DEFAULT NULL,
-  `ordering` int(11) DEFAULT '0',
-  `params` text,
-  PRIMARY KEY (`lost_object_id`),
-  UNIQUE KEY `slug` (`slug`)
+  PRIMARY KEY (`lost_object_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
